b200d2ab5cc8ba50ed488742abfbd2f5a4afc3ce
Aleksander <stdfire@gmail.com>
2024-12-16 18:33:15 +0200

    Задание 1

--- main.py
+++ main.py
@@ -19,49 +19,60 @@ for i in range (10):
     print(fild[i])
 
 
-pygame.init()
-class Tank():
-    def __init__(self, filename , hp, ammo, defence, range, color, x, y, l, h):
-        self.hp = hp
-        self.ammo = ammo
-        self.defence = defence
-        self.range = range
-        self.color = color
-        self.box = pygame.Rect(x, y, l, h)
-        self.picture = pygame.image.load(filename)
-    def fill(self):
-        pygame.draw.rect(mw, self.color, self.box)
-    def draw(self):
-        mw.blit(self.picture, (self.box.x, self.box.y))
+# pygame.init()
+# class Tank():
+#     def __init__(self, filename , hp, ammo, defence, range, color, x, y, l, h):
+#         self.hp = hp
+#         self.ammo = ammo
+#         self.defence = defence
+#         self.range = range
+#         self.color = color
+#         self.box = pygame.Rect(x, y, l, h)
+#         self.picture = pygame.image.load(filename)
+#     def fill(self):
+#         pygame.draw.rect(mw, self.color, self.box)
+#     def draw(self):
+#         mw.blit(self.picture, (self.box.x, self.box.y))
 
 
-mw = pygame.display.set_mode((800, 800))
-clock = pygame.time.Clock()
+# mw = pygame.display.set_mode((800, 800))
+# clock = pygame.time.Clock()
 
-in_game = True
+# in_game = True
 
-tank1 = Tank("images/tank1.png", 140, "pod", 60, 40, "gray", 40, 50, 20, 20)
-Tank2 = Tank("images/Tank2.png",200, "bp", 100, 30, "gray", 750, 750, 25, 25)
-# tank3 = Tank(160, "he", 70, 20, "gray", 400, 400, 20, 20)
-
-
-while in_game:
-    mw.fill("gray")
-
-    event_list = pygame.event.get()
-    for event in event_list:
-        if event.type == pygame.QUIT:
-            in_game = False 
-
-    tank1.draw()
-    Tank2.draw()
+# tank1 = Tank("images/tank1.png", 140, "pod", 60, 40, "gray", 40, 50, 20, 20)
+# Tank2 = Tank("images/Tank2.png",200, "bp", 100, 30, "gray", 750, 750, 25, 25)
+# # tank3 = Tank(160, "he", 70, 20, "gray", 400, 400, 20, 20)
 
 
+# while in_game:
+#     mw.fill("gray")
 
+#     event_list = pygame.event.get()
+#     for event in event_list:
+#         if event.type == pygame.QUIT:
+#             in_game = False 
 
-    pygame.display.update()
-    clock.tick(90)
+#     tank1.draw()
+#     Tank2.draw()
 
 
 
 
+#     pygame.display.update()
+#     clock.tick(90)
+
+
+# print("EndGame")
+
+line = 2
+idx = 5
+if fild[line][idx]==1:
+    print("yes")
+    # Рисуем мину
+else:
+    # рисуем пусое поле
+    print("no")
+
+
+#1 Отрисовать игровое поле 10 x 10 cразмерами квадрата 40х40
